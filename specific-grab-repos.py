d9cac67d6245dc6f7fdd6b3be53a192673db538e
Rijnard van Tonder <rvantonder@gmail.com>
2018-09-19 00:50:58 -0400

    uh. changes i never pushed

--- specific-grab-repos.py
+++ specific-grab-repos.py
@@ -4,12 +4,13 @@ import json
 import urllib2
 import sys
 
-DATE="2017-06-01"
+DATE="2017-06-01" # since push acvitiy and merge
 SIZE="500" # KB
-CONTRIBUTORS=5
-MERGED_PRS=1
+CONTRIBUTORS=5 # minimum contributors
+MERGED_PRS=1 # minimum merged prs
+# TODO: add threshold for time of merged pr
 
-repos_url = "https://api.github.com/search/repositories?q=language:%s+pushed:>"+DATE+"+size:<="+SIZE+"&sort=stars&order=desc&page=%d&per_page=1000"
+repos_url = "https://api.github.com/search/repositories?q=language:%s+pushed:>"+DATE+"+size:>"+SIZE+"&sort=stars&order=desc&page=%d&per_page=1000"
 
 def access(url):
   f = open("token.txt", "r")
@@ -62,7 +63,7 @@ def filter_has_contributors_and_merged_prs(repos):
 
 if __name__ == '__main__':
   if len(sys.argv) == 1:
-    print "Usage: ./grap-repos.py <language> <number (max = 1000)>"
+    print "Usage: ./grap-repos.py <language> <number (max = 999)>"
   else:
     lang = sys.argv[1]
     number = int(sys.argv[2])
@@ -70,7 +71,7 @@ if __name__ == '__main__':
     repos = map(lambda i: popular_repos(repos_url % (lang,i)), range(1,pages+1))
 
     repos = flatten(repos)
-    repos = filter_has_contributors_and_merged_prs(repos[:100])
+    repos = filter_has_contributors_and_merged_prs(repos)
 
     print "~=~=~=~=~=~"
     for i in xrange(len(repos)):
