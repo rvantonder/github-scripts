5f2917d023fe796372a33c51c9253d723e58610a
Rijnard van Tonder <rvantonder@gmail.com>
2018-10-01 23:29:59 -0400

    more processes for mass clone

--- mass-clone.sh
+++ mass-clone.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat | xargs -P 5 -L 1 -I @ bash -c "git clone @ \`basename '@'\`-\`date +\"%Y-%m-%d-%H-%M-%S\"\`" 
+cat | xargs -P 10 -L 1 -I @ bash -c "git clone @ \`basename '@'\`-\`date +\"%Y-%m-%d-%H-%M-%S\"\`" 
